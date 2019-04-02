9a4ce6bf83533cb9e1b546422381391099dd8dc7
tafewollongbarsys <39718635+tafewollongbarsys@users.noreply.github.com>
2019-04-02 15:58:02 +1100

    Initial commit

---
+++ rename_pcs/rename-pcs.ps1
@@ -0,0 +1,7 @@
+﻿### BATCH RENAME PCs
+# Open modal to ask for admin credentials
+$admincredentials = Get-Credential
+# Set variable - import data from CSV
+$list = Import-Csv c:\scripts\list.csv -Header OldName,NewName
+# For each item in OldName column in the CSV, rename using the NewName column.
+Foreach ( $pc in $list ) {Rename-Computer -ComputerName $pc.OldName -NewName $pc.NewName -DomainCredential $admincredentials -Force -Restart}
