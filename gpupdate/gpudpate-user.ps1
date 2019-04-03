50b483901543c6a7d2016c0287c1a698b5fabe8e
tafewollongbarsys <39718635+tafewollongbarsys@users.noreply.github.com>
2019-04-03 14:22:29 +1100

    Create gpudpate-user.ps1

---
+++ gpupdate/gpudpate-user.ps1
@@ -0,0 +1,9 @@
+### BATCH RENAME PCs
+# Open modal to ask for admin credentials. Set these as a variable.
+$admincredentials = Get-Credential
+# Set variable - import data from CSV
+$list = Import-Csv c:\scripts\hostlist.csv
+# For each item in the CSV, run Group Policy Update immediately
+Foreach ( $pc in $list ) {
+  Invoke-GPUpdate –Computer $pc –RandomDelayInMinutes 0 -Target "User" -Force
+}
