82d250a19585f5d25e89f6f35eeb7965dce6c0f6
tafewollongbarsys <39718635+tafewollongbarsys@users.noreply.github.com>
2019-04-03 14:27:05 +1100

    Update rename-pcs.ps1

--- rename_pcs/rename-pcs.ps1
+++ rename_pcs/rename-pcs.ps1
@@ -2,6 +2,7 @@
 # Open modal to ask for admin credentials. Set these as a variable.
 $admincredentials = Get-Credential
 # Set variable - import data from CSV
+# Change this path as needed.
 $list = Import-Csv c:\scripts\list.csv -Header OldName,NewName
 # For each item in OldName column in the CSV, rename using the NewName column.
 Foreach ( $pc in $list ) {Rename-Computer -ComputerName $pc.OldName -NewName $pc.NewName -DomainCredential $admincredentials -Force -Restart}
